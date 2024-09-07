d31eae6d6c53bf80ccb5c5201e83468b4281a56d
Rahul Raj <raj.rahul686@gmail.com>
2024-09-08 03:27:43 +0530

    add event listner on each item and use console in callback function with parenthesis

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,5 +1,18 @@
+const handleClick = ({ item }) => {
+  alert(item);
+};
 const TodoItem = ({ item, id }) => {
-  return <li key={id}>{item}</li>;
+  return (
+    <li
+      key={id}
+      onClick={(e) => {
+        console.log(e);
+        handleClick({ item });
+      }}
+    >
+      {item}
+    </li>
+  );
 };
 
 export default TodoItem;
